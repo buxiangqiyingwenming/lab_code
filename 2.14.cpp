9f417162ad35f78e6c3de731791ff9d60735e983
buxiangqiyingwenming <43210739+buxiangqiyingwenming@users.noreply.github.com>
2018-10-21 20:24:10 +0800

    Add files via upload

---
+++ 2.14.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	void *vp;
+	char c = 'A';
+	short int x = 97;
+	vp = &c;             		
+	cout << *(char *)vp << "  " 
+		<< *(short int*)vp << endl;
+	vp = &x;           
+	cout << *(short int *)vp << "  " 
+		<< *(char*)vp << endl;
+	system("pause");
+
+	return  0;
+}
